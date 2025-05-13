3465be6340eb7548f5a6f8266b5cf1cd5b59d74f
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2025-05-13 16:51:11 -0700

    start code for icache

---
+++ ucsbece154_icache.v
@@ -0,0 +1,27 @@
+module ucsbece154b_icache #(
+    parameter NUM_SETS   = 8,
+    parameter NUM_WAYS   = 4,
+    parameter BLOCK_WORDS= 4,
+    parameter WORD_SIZE  = 32
+)(
+    input                     Clk,
+    input                     Reset,
+
+    // core fetch interface
+    input                     ReadEnable,
+    input      [31:0]         ReadAddress,
+    output reg [WORD_SIZE-1:0]Instruction,
+    output reg                Ready,
+    output reg                Busy,
+
+    // SDRAM-controller interface
+    output reg [31:0]         MemReadAddress,
+    output reg                MemReadRequest,
+    input      [31:0]         MemDataIn,
+    input                     MemDataReady
+);
+
+// implementation of the cache here
+
+
+endmodule
